c4b8d63b716a7069207ef2b1ac93e94fb2841c6b
sysbot <sysbot@kali>
2024-06-04 01:26:59 -0400

    add some features and refactor

--- app.py
+++ app.py
@@ -7,18 +7,16 @@ app = FastAPI()
 with open("elements.json", "r") as file:
     tabla_periodica_data = json.load(file)
 
-@app.get("/elementos/")
+@app.get("/elements/")
 async def listar_elementos():
-    """Devuelve una lista de todos los elementos de la tabla periódica"""
     return tabla_periodica_data
 
-@app.get("/elementos/{numero_atomico}")
+@app.get("/elements/{numero_atomico}")
 async def obtener_elemento(numero_atomico: int):
-    """Devuelve los datos de un elemento específico según su número atómico"""
     for elemento in tabla_periodica_data:
         if elemento["atomic_number"] == str(numero_atomico):
             return elemento
-    raise HTTPException(status_code=404, detail="Elemento no encontrado")
+    raise HTTPException(status_code=404, detail="Elemento no encontrado / Not Found")
 
 if __name__ == "__main__":
     import uvicorn
